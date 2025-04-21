47b2a7aba30500bbe30aa6f90ad724bd8b2ad224
Marco Winter <marco.winter@enviaM.de>
2025-04-21 13:44:25 +0200

    first version of search bar component

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webcomponent-collection
-Webcomponent Collection
+A collection of reusable web components as single module files.
+
+## Search Bar
+A component that renders an input field for search text, and a button to trigger a filter search for DOM elements on the page. 
+
+The following markup will generate a filter component for list items of an unordered list with the id `item-list`.
+
+```
+<search-bar item-selector="ul#item-list li" placeholder="Search text" button-text="Search!"></search-bar>
+```
---
+++ src/components/searchbar.js
@@ -0,0 +1,104 @@
+/**
+ * This class contains render and filter logic for an on-site search bar
+ * that filters DOM elements by their properties
+ */
+class SearchBar extends HTMLElement 
+{
+    constructor() {
+        super();
+    }
+    /**
+     * this method is called when the element is added to the document
+     * that means usually it's called once.
+     * Use this for initialisation logic
+     * @returns { void }
+     */
+    connectedCallback() 
+    {
+        this.shadow = this.attachShadow({mode: 'closed'});
+
+        const style = document.createElement("style");
+        style.textContent = `
+:host {    
+    margin: 4px;
+    --border-color: grey;
+    --button-color: grey;
+    --button-hover-color: grey;
+    --font-size: 1.1rem;
+}
+
+input {
+    font-size: var(--font-size);
+    margin-right: 0.5rem;
+    padding: 8px;
+    border:1px solid;
+    border-color:var(--border-color);
+    border-radius: 0.3rem;
+}
+
+button {
+    font-size: var(--font-size);
+    padding: 8px;
+    border:1px solid;
+    border-color:var(--border-color);
+    border-radius: 0.3rem;
+    background-color: var(--button-color);
+}
+button:hover {
+    background-color: var(--button-hover-color);
+}
+        `;
+        this.shadow.appendChild(style);
+
+        const container = document.createElement("div");
+
+        const textInput = document.createElement("input");
+        textInput.placeholder = this.getAttribute('placeholder') ?? "";
+        container.appendChild(textInput);
+
+        const searchButton = document.createElement("button");
+        searchButton.textContent = this.getAttribute('button-text') ?? "Search";
+        searchButton.addEventListener('click', () => this.filterItems(textInput.value));
+        container.appendChild(searchButton);
+        
+        this.shadow.appendChild(container);
+    }
+
+    filterItems(searchTerm)
+    {
+        const search = new RegExp(searchTerm, "ig");
+        Array.from(document.querySelectorAll(this.getAttribute('item-selector')))
+            .forEach(item => {                
+                if(item.style.display !== 'none' && item.textContent.match(search) === null)
+                {   
+                    item.style.display = 'none';
+                }                 
+                else if(item.style.display === 'none' && item.textContent.match(search) !== null)
+                {
+                    item.style.display = '';
+                }
+            });
+    }
+    
+    /**
+     * this read-only property provides information to the browser 
+     * which component properties should trigger the attributesChangedCallback method
+     * @returns { String[] }
+     */
+    static get observedAttributes() 
+    {
+        return [];
+    }
+
+    // 
+    /**
+     * handler for changing attributes, called by the browser
+     * @param { String } property name of the updated component property
+     * @param { String } oldValue previous value 
+     * @param { String } newValue new value that needs to be set in this method
+     */
+    attributeChangedCallback(property, oldValue, newValue)
+    {
+    }
+}
+customElements.define('search-bar', SearchBar);
